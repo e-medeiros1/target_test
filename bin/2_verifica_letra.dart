456944fa949c3d9cde8319182b227bd2c2a4a508
Erilândio Santos Medeiros <hsymetry@gmail.com>
2024-09-18 20:31:07 -0300

    Add question 2: "Verifica letra" to repository

---
+++ bin/2_verifica_letra.dart
@@ -0,0 +1,25 @@
+//Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula,
+//além de informar a quantidade de vezes em que ela ocorre.
+
+import 'dart:io';
+
+void main() {
+  print('Digite uma string: ');
+  String input = stdin.readLineSync()!;
+
+  //Tratamento de erros
+  if (input.isEmpty) {
+    print('Digite uma palavra');
+    return;
+  }
+
+  String converteParaMinusculo = input.toLowerCase();
+
+  int contagem = converteParaMinusculo.split('a').length - 1;
+
+  if (contagem > 0) {
+    print('A letra "a" aparece $contagem vezes na string.');
+  } else {
+    print('A letra "a" não foi encontrada na string.');
+  }
+}
