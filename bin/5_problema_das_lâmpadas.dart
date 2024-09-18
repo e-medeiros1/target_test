38be1323b438250e321f233309e7966451c085df
Erilândio Santos Medeiros <hsymetry@gmail.com>
2024-09-18 20:32:39 -0300

    Add question 5: "O problema das lâmpadas" to repository

---
+++ bin/5_problema_das_lâmpadas.dart
@@ -0,0 +1,14 @@
+/* 
+5) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes.
+Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. 
+Seu objetivo é descobrir qual interruptor controla qual lâmpada. 
+Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?  
+*/
+
+//Acenderia o primeiro por um determinado tempo e desligaria, depois o segundo e deixaria aceso, 
+//se a sala que eu for a lâmpada tiver apagada MAS quente, corresponde ao primeiro interruptor,
+//se tiver acesa, ao B e se tiver apagada E fria, corresponde ao C.  
+
+//Essa é velha kkkk
+
+//Erilândio Santos Medeiros
